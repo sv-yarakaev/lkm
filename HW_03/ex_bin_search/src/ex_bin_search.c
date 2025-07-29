f815bbb787d1de6c3a91fa02181a77b5dd01f6d3
Stanislav <yarakaeff@gmail.com>
2025-07-29 14:29:56 +0300

    add parameters without checking

--- HW_03/ex_bin_search/src/ex_bin_search.c
+++ HW_03/ex_bin_search/src/ex_bin_search.c
@@ -1,3 +1,12 @@
+/*
+    Использование bsearch.
+    Поиск Pid процесса. Массив pid формируется в момент старта модуля.
+    Есть два параметра для поиска pid: extern_pid и output_string
+
+    Замечание. Массив не сортируется, так как  заполняется уже отсортированым
+
+*/
+#include "linux/kern_levels.h"
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/moduleparam.h>
 #include <asm-generic/errno-base.h>
@@ -12,16 +21,58 @@
 #include <linux/random.h>
 #include <linux/types.h>
 #include <linux/sched.h>
+#include <linux/bsearch.h>
+#include <linux/kstrtox.h>
 
-const char *find_pid = "Find PID= %d ";
+
+char *find_pid = "Find PID= %d ";
 const char *notfind_pid = "PID %d not found";
 static char output_string[64] = "Enter PID to extern_pid to find";
 
+static int *array = NULL;
+static int common_size;
+
 static int extern_pid = -1;
+
+static int compare_pids(const void *a, const void *b);
+static int get_put(char *buffer, const struct kernel_param *kp);
+
+
+static int compare_pids(const void *a, const void *b) {
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+    if (arg1 > arg2) 
+        return -1;  
+    if (arg1 < arg2) 
+        return 1;
+    return 0;
+}
+
+
+
+
 static int pid_search(const char *val, const struct kernel_param *kp) {
+
+    int find_pid;
+    int ret = kstrtoint(val, 10, &find_pid);
+    int *result = (int *) bsearch(&ret, array, common_size, sizeof(int), compare_pids); 
+
+    if (result != NULL) {
+        //char buf[100];
+        snprintf(output_string,sizeof(output_string), "Find PID = %d ", ret);
+        get_put(output_string, kp);
+        printk(KERN_INFO "Find PID = %d \n", ret);
+
+
+    } else {
+        snprintf(output_string,sizeof(output_string), "Not found PID = %d ", ret);
+        get_put(output_string, kp);
+        printk(KERN_INFO "PID = %d not found \n", ret);
+    }
 
     return 0;
 }
+
 
 static const struct kernel_param_ops search_pid = {
     .set = pid_search,
@@ -29,14 +80,14 @@ static const struct kernel_param_ops search_pid = {
 };
 
 
-
-static int *array = NULL;
-
 struct pids_snap {
     int pid;
     struct llist_node list;
 };
-static LLIST_HEAD(my_list);
+static LLIST_HEAD(pids_list);
+
+
+
 
 
 static int count_processes(void) {
@@ -44,18 +95,21 @@ static int count_processes(void) {
     int count = 0;
 
     rcu_read_lock();
+
     //NOLINTNEXTLINE(clang-analyzer-sizeof-pointer)
     for_each_process(task) {
         struct pids_snap *local = kmalloc(sizeof(struct pids_snap), GFP_KERNEL);
         if (!local) {
-            continue; // заменить
+            continue; // заменить 
         }
         local->pid = task->pid;
-        llist_add(&local->list, &my_list);
+        llist_add(&local->list, &pids_list);
         count++;
     }
     rcu_read_unlock();
 
+    common_size = count;
+       
     return count;
 }
 
@@ -63,7 +117,7 @@ static void free_pid_list(void) {
     struct llist_node *node, *tmp;
     struct pids_snap *entry;
 
-    llist_for_each_safe(node, tmp, llist_del_all(&my_list)) {
+    llist_for_each_safe(node, tmp, llist_del_all(&pids_list)) {
         entry = container_of(node, struct pids_snap, list);
         pr_info("Freeing PID %d\n", entry->pid);
         kfree(entry);
@@ -79,9 +133,10 @@ static int create_array(void) {
         pr_err("kmalloc failed, cannot create array");
         return -ENOMEM;
     }
-    llist_for_each_entry(entry, my_list.first, list) {
-        printk(KERN_INFO "Add PID: %d\n", entry->pid);
+    llist_for_each_entry(entry, pids_list.first, list) {
+        printk(KERN_INFO "%d -> Add PID: %d\n", i, entry->pid);
         array[i] = entry->pid;
+        i++;
     }
     return 0;
     
