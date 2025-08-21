a4e718468f4fbede4d1232f3a0e5e759c61bb8fb
Stanislav <yarakaeff@gmail.com>
2025-08-21 16:59:21 +0300

    add pr_fmt and change headers

--- HW_03/ex_queue/src/ex_queue.c
+++ HW_03/ex_queue/src/ex_queue.c
@@ -1,6 +1,7 @@
-#include "linux/gfp_types.h"
-#include "linux/list.h"
-#include "linux/slab.h"
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#include <linux/gfp_types.h>
+#include <linux/list.h>
+#include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 
