2b637e0f436eea9fc7208dae1b19e3551b5d6af4
Stanislav <yarakaeff@gmail.com>
2025-08-22 14:23:39 +0300

    fix memory free

--- HW_03/ex_bitmap/src/ex_bitmap.c
+++ HW_03/ex_bitmap/src/ex_bitmap.c
@@ -1,4 +1,6 @@
-#include "linux/printk.h"
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#include <linux/gfp_types.h>
+#include <linux/printk.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/bitmap.h>
@@ -6,24 +8,18 @@
 
 #define NR_ARRAY 2
 
-enum S{
-    FIRST = 1,
-    SECOND,
-    THIRD,
-
-};
 
 unsigned long bitmap_array[NR_ARRAY];
 
 static char* bin_view(unsigned long num) {
-    static char buffer[sizeof(unsigned long) * 8 + 1]; 
-    int bits = sizeof(num) * 8;
+    char* buffer = kmalloc(sizeof(unsigned long) * 8 + 1, GFP_KERNEL); 
+    if (!buffer) return NULL; 
     
+    int bits = sizeof(num) * 8;
     for (int i = 0; i < bits; i++) {
         buffer[i] = (num & (1UL << (bits - 1 - i))) ? '1' : '0';
     }
-    buffer[bits] = '\0'; 
-    
+    buffer[bits] = '\0';
     return buffer;
 }
 
@@ -47,7 +43,11 @@ static void check_first_bit(void) {
     for(int i = 0; i < NR_ARRAY; i++) {
         if (test_bit(0, &bitmap_array[i])) {
             pr_info("First bit is setup. Value 0x%lx\n", bitmap_array[i]);
-            pr_info("Value is %s\n", bin_view(bitmap_array[i]));
+            char *str = bin_view(bitmap_array[i]); 
+            if (str) {
+                pr_info("Value is %s\n", str); 
+                kfree(str); 
+            }
         } else {
             pr_warn("First bit isn't setup. Value 0x%lx\n", bitmap_array[i]);
         }
@@ -60,7 +60,11 @@ static __init int init_ex_bitmap(void) {
     pr_info("Create bitmap array\n");
     for (int i = 0; i < NR_ARRAY; i++) {
         bitmap_array[i] = get_random_ulong();
-        pr_info("Value is %s\n", bin_view(bitmap_array[i]));
+        char *str = bin_view(bitmap_array[i]); 
+        if (str) {
+            pr_info("Value is %s\n", str);
+            kfree(str); 
+        }
     }
     set_first_bit();
     check_first_bit();
@@ -68,10 +72,9 @@ static __init int init_ex_bitmap(void) {
     return 0;
 }
 
-static __exit void exit_ex_bitmap(void) {}
-
-
-
+static __exit void exit_ex_bitmap(void) {
+    pr_info("Bb\n");
+}
 
 
 
