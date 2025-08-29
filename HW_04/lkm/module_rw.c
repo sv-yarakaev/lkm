2d2b73d00451ed57e3371cc7b44ee194fa17bac4
Stanislav <stv@hbuild.minimax.su>
2025-08-29 23:51:04 +0300

    empty projects

--- HW_04/lkm/module_rw.c
+++ HW_04/lkm/module_rw.c
@@ -1,9 +1,13 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-
-
-
+static __init int rw_init(void) {
+  printk(KERN_INFO "EHLO\n");
+  return 0;
+}
+static __exit void rw_exit(void) { printk(KERN_INFO "BB\n"); }
 
 module_init(rw_init);
 module_exit(rw_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Homework 04 ");
