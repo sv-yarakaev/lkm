2222ead35a504ecf1514c76a5ef974fb17e00534
Stanislav <yarakaeff@gmail.com>
2025-09-08 15:36:25 +0300

    remove all pthread context

--- HW_04/app_us/main.c
+++ HW_04/app_us/main.c
@@ -1,17 +1,11 @@
+#include "ADT/linked_list.h"
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ADT/linked_list.h"
-#include <pthread.h>
 #include <time.h>
 #include <unistd.h>
-
-typedef struct record_db {
-  char *name;
-  long id;
-} record_db;
-
 
 typedef struct simple_ll_db {
   char *name;
@@ -21,74 +15,45 @@ typedef struct simple_ll_db {
 
 static LIST_HEAD(g_records);
 
-/* Инициализация головного списка вручную (если нужен явный вызов)
-   И не нужна, если используется LIST_HEAD(g_records) выше.
-*/
-static void record_db_init_list(void) {
-    INIT_LIST_HEAD(&g_records);
-}
+static void record_db_init_list(void) { INIT_LIST_HEAD(&g_records); }
 
+static record_db_t *record_db_new(const char *name, long id) {
+  record_db_t *rec = malloc(sizeof(*rec));
+  if (!rec)
+    return NULL;
 
-/* Вспомогательные функции управления элементом */
-static record_db_t *record_db_new(const char *name, long id)
-{
-    record_db_t *rec = malloc(sizeof(*rec));
-    if (!rec)
-        return NULL;
-
-    rec->name = strdup(name ? name : "");
-    if (!rec->name) {
-        free(rec);
-        return NULL;
-    }
-    rec->id = id;
-    // Для обычных элементов инициализировать rec->node не обязательно
-    return rec;
-}
-
-
-
-
-/* Добавление элемента в хвост списка */
-static int record_db_add(const char *name, long id)
-{
-    record_db_t *rec = record_db_new(name, id);
-    if (!rec)
-        return -1;
-
-    list_add_tail(&rec->node, &g_records);
-    return 0;
-}
-static void record_db_free(record_db_t *rec)
-{
-    if (!rec) return;
-    free(rec->name);
+  rec->name = strdup(name ? name : "");
+  if (!rec->name) {
     free(rec);
+    return NULL;
+  }
+  rec->id = id;
+  return rec;
 }
 
-/* Полная очистка списка */
-static void record_db_clear(void)
-{
-    record_db_t *pos, *n;
+static int record_db_add(const char *name, long id) {
+  record_db_t *rec = record_db_new(name, id);
+  if (!rec)
+    return -1;
 
-    list_for_each_entry_safe(pos, n, &g_records, node) {
-        list_del(&pos->node);
-        record_db_free(pos);
-    }
+  list_add_tail(&rec->node, &g_records);
+  return 0;
 }
-
-
+static void record_db_free(record_db_t *rec) {
+  if (!rec)
+    return;
+  free(rec->name);
+  free(rec);
+}
 
 
 static int count = 42;
 #define NUM_NAMES 24
 #define NAME_LENGTH 20
-struct record_db *simple_db;
 static pthread_rwlock_t db_lock;
 #define READERS 5
 #define WRITERS 2
 
-static record_db *write_row();
 static void *writer_thread(void *args);
 static void *reader_thread(void *args);
 static volatile int running = 1;
@@ -101,48 +66,89 @@ const char *random_names[] = {
 
 static void init_db() {
   srand(time(NULL));
-  simple_db = (struct record_db *)malloc(sizeof(struct record_db) * count);
   for (int i = 0; i < count; i++) {
-    simple_db[i] = *write_row();
-    record_db_add(simple_db[i].name, simple_db[i].id);
+    int name_index = rand() % NUM_NAMES;
+    char *name = (char *)malloc(NAME_LENGTH * sizeof(char));
+    snprintf(name, NAME_LENGTH, "%s", random_names[name_index]); 
+    long id = 1000 + rand() % 9000;
+    record_db_add(name, id);
+    free(name);
   }
 }
-
-static record_db *write_row() {
-  struct record_db *row = (record_db *)malloc(sizeof(record_db));
-  int name_index = rand() % NUM_NAMES;
-  row->name = (char *)malloc(NAME_LENGTH * sizeof(char));
-  snprintf(row->name, NAME_LENGTH, "%s", random_names[name_index]);
-  row->id = 1000 + rand() % 9000;
-  return row;
-}
-
 
 
 static void free_db() {
-  for (int i = 0; i < count; i++) {
-    if (simple_db[i].name != NULL) {
-      free(simple_db[i].name);
+   record_db_t *pos, *n;
+    list_for_each_entry_safe(pos, n, &g_records, node) {
+        list_del(&pos->node);
+        record_db_free(pos);
     }
-  }
-  free(simple_db);
-  simple_db = NULL;
 }
 
-struct record_db read_row_db(int row) { return simple_db[row]; }
+static record_db_t *record_db_find_by_id(long id)
+{
+    record_db_t *pos;
+
+    list_for_each_entry(pos, &g_records, node) {
+        if (pos->id == id) {
+            return pos;
+        }
+    }
+    return NULL;
+}
+
+
+
+/* Поиск по имени */
+static record_db_t *record_db_find_by_name(const char *name)
+{
+    record_db_t *pos;
+
+    if (!name)
+        return NULL;
+
+    list_for_each_entry(pos, &g_records, node) {
+        if (strcmp(pos->name, name) == 0) {
+            return pos;
+        }
+    }
+    return NULL;
+}
 
 static void print_db() {
   printf("=== Local Simple DataBase ===\n");
-  for (int i = 0; i < count; i++) {
-    printf("Record %d: ID = %ld, Name = %s\n", i, simple_db[i].id,
-           simple_db[i].name);
-  }
-  printf("===============================\n");
   record_db_t *pos;
 
+  list_for_each_entry(pos, &g_records, node) {
+    printf("record: id=%ld name=%s\n", pos->id, pos->name);
+  }
+  printf("===============================\n");
+}
+
+static int record_db_update_by_id(long old_id, long new_id, const char *new_name)
+{
+    record_db_t *pos;
+    char *dup;
+    const char *nm = new_name ? new_name : "";
+
+    dup = strdup(nm);
+    if (!dup)
+        return -1;
+
     list_for_each_entry(pos, &g_records, node) {
-        printf("record: id=%ld name=%s\n", pos->id, pos->name);
+        if (pos->id == old_id) {
+            char *old = pos->name;
+
+            pos->id = new_id;
+            pos->name = dup;
+
+            free(old);           
+            return 0;
+        }
     }
+
+    free(dup);                 
+    return -1;
 }
 
 
@@ -153,7 +159,7 @@ int main(void) {
 
   */
   print_db();
-
+/*
   if (pthread_rwlock_init(&db_lock, NULL)) {
     perror("cannot make lock init");
     return 1;
@@ -173,13 +179,12 @@ int main(void) {
       return 1;
     }
   }
-  
+*/
   free_db();
   return 0;
 }
 
 static void *reader_thread(void *args) {
-  (void)args;
   unsigned int seed =
       (unsigned int)time(NULL) ^ (unsigned int)(uintptr_t)pthread_self();
   char name_copy[NAME_LENGTH];
@@ -187,11 +192,11 @@ static void *reader_thread(void *args) {
     int idx = rand_r(&seed) % count;
 
     pthread_rwlock_rdlock(&db_lock);
-    long id = simple_db[idx].id;
-    snprintf(name_copy, sizeof(name_copy), "%s", simple_db[idx].name);
+    record_db_t* id = record_db_find_by_id(idx);
+    snprintf(name_copy, sizeof(name_copy), "%s", id->name);
     pthread_rwlock_unlock(&db_lock);
 
-    printf("[R] read idx=%d name=%s id=%ld\n", idx, name_copy, id);
+    printf("[R] read idx=%d name=%s id=%ld\n", idx, name_copy, id->id);
 
     usleep(50 * 1000); // 50 ms
   }
@@ -216,20 +221,21 @@ static void *writer_thread(void *args) {
 
     pthread_rwlock_wrlock(&db_lock);
 
-    char *old_name = simple_db[idx].name;
-    long old_id = simple_db[idx].id;
 
-    simple_db[idx].name = new_name;
-    simple_db[idx].id = new_id;
+    record_db_t *old_id = record_db_find_by_id(idx);
+
+    record_db_update_by_id(idx, new_id,  new_name);
+
 
     pthread_rwlock_unlock(&db_lock);
 
-    free(old_name);
 
-    printf("[W] write idx=%d %ld/%s -> %ld/%s\n", idx, old_id, "(old)", new_id,
+    printf("[W] write idx=%d %ld/%s -> %ld/%s\n", idx, old_id->id, "(old)", new_id,
            new_name);
 
     usleep(120 * 1000); // 120 ms
+    free(new_name);
+
   }
   return NULL;
 }
