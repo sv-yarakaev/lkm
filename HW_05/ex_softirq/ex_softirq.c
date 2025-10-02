ba059f10448064c6d26d3a314ee0aab8097d97c1
Stanislav <stv@hbuild.minimax.su>
2025-10-02 22:38:14 +0300

    empty softirq

--- HW_05/ex_softirq/ex_softirq.c
+++ HW_05/ex_softirq/ex_softirq.c
@@ -1,43 +1,22 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/proc_fs.h>
 
 
-#define LOCAL_SOFTIRQ 29
-
-static void cb_softirq_action(struct softirq_action *h)
-{
-	pr_info("cb_softirq_action() executing in softirq context (CPU %d)\n", smp_processor_id());
-}
 
 static int __init ex_softirq_init(void) {
-    
-    pr_info("Load module.\n");
 
-    init_irq_work(&my_irq_work, my_irq_work_handler);
-    pr_info("Init irq_work.\n");
+  pr_info("Load module.\n");
 
-    // create proc for raising
-    proc_entry = proc_create("ex_softirq", 0666, NULL, &proc_fops);
-    if (!proc_entry) {
-        pr_err("Cannot create /proc/ex_softirq\n");
-        return -ENOMEM;
-    }
-
-    pr_info("Run: echo raise | sudo tee /proc/ex_softirq\n");
-    return 0; 
-
-
-
+  return 0;
 }
 
 static void __exit ex_softirq_exit(void) {
-    pr_info("Example ex_softirq: BB n exit.\n");
-
+  pr_info("BB n exit.\n");
 }
 
 module_init(ex_softirq_init);
@@ -45,4 +24,4 @@ module_exit(ex_softirq_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Stv");
-MODULE_DESCRIPTION("HW 05 TIMER_SOFTIRQ");
+MODULE_DESCRIPTION("HW 05 Empty SOFTIRQ");
