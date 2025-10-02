ee28bfb56eeb1f1ba1b5aff904ec5f65f27710a7
Stanislav <stv@hbuild.minimax.su>
2025-10-02 22:06:10 +0300

    tasklets

--- HW_05/ex_tasklets/ex_tasklets.c
+++ HW_05/ex_tasklets/ex_tasklets.c
@@ -1,36 +1,38 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/interrupt.h> 
-#include <linux/jiffies.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
 
-
-static void cb_tasklet_local(unsigned long data);
-DECLARE_TASKLET(my_tasklet, cb_tasklet_local, 0);
-
-static void cb_tasklet_local(unsigned long data)
-{
-	pr_info("cb_tasklet_local running in softirq context on CPU %d, data=%lu\n",
-	        smp_processor_id(), data);
+static void cb_tasket_func(unsigned long data) {
+    pr_info("Tasklet function executed with data: %lu and on cpu: %d \n", data, smp_processor_id());
 }
 
-static int __init ex_tasklets_init(void)
-{
-	pr_info("Init - scheduling tasklet\n");
-	tasklet_schedule(&my_tasklet);
+static struct tasklet_struct local_tasket;
 
-	pr_info("Scheduling another tasklet run in 1 second\n");
-	mdelay(10);
-	tasklet_schedule(&my_tasklet);
+static int __init ex_tasklets_init(void) {
+    pr_info("Module init\n");
 
-	return 0;
+    tasklet_init(&local_tasket, cb_tasket_func, 42UL);  
+    pr_info("Tasklet initialized\n");
+
+    tasklet_schedule(&local_tasket);
+    pr_info("Tasklet scheduled (normal priority)\n");
+
+    tasklet_hi_schedule(&local_tasket);
+    pr_info("Tasklet scheduled (high priority)\n");
+
+
+    pr_info("Simulating IRQ bottom-half usage\n");
+    tasklet_schedule(&local_tasket);
+
+    return 0;
 }
 
-static void __exit ex_tasklets_exit(void)
-{
-	pr_info("exit - killing tasklet\n");
-	tasklet_kill(&my_tasklet);
-	pr_info("tasklet killed\n");
+static void __exit ex_tasklets_exit(void) {
+    tasklet_kill(&local_tasket);
+    pr_info("Tasklet killed on exit\n");
+    pr_info("Module exiting\n");
 }
 
 module_init(ex_tasklets_init);
