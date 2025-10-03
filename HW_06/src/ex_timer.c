577db31ca0043032cc541a6c0b0ca888c5dbc9cb
Stanislav <yarakaeff@gmail.com>
2025-10-03 15:52:09 +0300

    fill init

--- HW_06/src/ex_timer.c
+++ HW_06/src/ex_timer.c
@@ -1,8 +1,25 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+#include <linux/param.h> //for HZ
+                     
+static struct timer_list my_timer;
+static unsigned int counter = 0;
+statuc unsigned int timeout_ms; // automatic define interval
+
+
 
 
 static __init int ex_timer_init(void) {
+    timeout_ms = 1000 / HZ; 
+    pr_info("Module loaded. HZ = %d (tick is every %u ms)\n", HZ, timeout_ms);
+
+    timer_setup(&my_timer, my_timer_callback, 0);
+
+    mod_timer(&my_timer, my_timer_callback, 0);
+
     return 0;
 }
 
