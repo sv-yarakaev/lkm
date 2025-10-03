57bafe754cf894673f82cfb07584ddeb252217c9
Stanislav <yarakaeff@gmail.com>
2025-10-03 15:58:56 +0300

    with error in timer

--- HW_06/src/ex_timer.c
+++ HW_06/src/ex_timer.c
@@ -1,3 +1,4 @@
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -7,10 +8,15 @@
                      
 static struct timer_list my_timer;
 static unsigned int counter = 0;
-statuc unsigned int timeout_ms; // automatic define interval
+static unsigned int timeout_ms; // automatic define interval
 
 
+static void my_timer_callback(struct timer_list *timer) {
+    counter++;
+    pr_info("Tick %u, HZ = %d, timeout = %u, jiffies = %lu\n", counter, HZ, timeout_ms, jiffies);
 
+    mod_timer(timer, jiffies + msecs_to_jiffies(timeout_ms));
+}
 
 static __init int ex_timer_init(void) {
     timeout_ms = 1000 / HZ; 
@@ -23,7 +29,10 @@ static __init int ex_timer_init(void) {
     return 0;
 }
 
-static __exit void ex_timer_exit(void) {}
+static __exit void ex_timer_exit(void) {
+    del_timer_sync(&my_timer);
+    pr_info("Module is unloaded\n");
+}
 
 
 module_init(ex_timer_init);
