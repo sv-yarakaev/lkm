4a03abd5a7f2b3b2721893cac49206feaa43c861
Stanislav <yarakaeff@gmail.com>
2025-10-06 16:11:06 +0300

    simple kmalloc

---
+++ HW_07_mem/ex_kmalloc/ex_kmalloc.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/ktime.h>
+#include <linux/mm.h>
+
+
+static int __init ex_init(void) {
+    size_t size = 1024;
+    void *ptr = NULL;
+    ktime_t start, end;
+    s64 delta;
+    while (size < (1UL << 32)) {  
+        printk(KERN_INFO "kmalloc: %zu byte\n", size);
+        start = ktime_get();
+        ptr = kmalloc(size, GFP_KERNEL);
+        end = ktime_get();
+        delta = ktime_to_ns(ktime_sub(end, start)) / 1000000;  // ns to ms
+        if (ptr) {
+            printk(KERN_INFO "kmalloc: SUCCESS\n");
+            printk(KERN_INFO "kmalloc: %zu byte, %lld ms, type: physical contiguous\n", size, delta);
+            kfree(ptr);
+            size *= 2;
+        } else {
+            pr_crit("kmalloc: FAIL, err_msg = out of memory\n");
+            break;
+        }
+    }
+    return 0;
+}
+
+static void __exit ex_exit(void) {
+}
+
+module_init(ex_init);
+module_exit(ex_exit);  
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Stv");
+MODULE_DESCRIPTION("HW_07 kmalloc allocation");
+
+
