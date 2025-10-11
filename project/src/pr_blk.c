2e79c0f07f9ff2625564cf646fa131d46f9a9e7b
Stanislav <stv@hbuild.minimax.su>
2025-10-11 14:03:45 +0300

    full auth

--- project/src/pr_blk.c
+++ project/src/pr_blk.c
@@ -1,10 +1,11 @@
+#include "linux/numa.h"
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include "asm-generic/int-ll64.h"
 #include "linux/radix-tree.h"
 #include "linux/spinlock_types.h"
 
-
+#include <linux/debugfs.h>
 #include <linux/module.h>
 #include <linux/blkdev.h>
 #include <linux/blk-mq.h>
@@ -32,9 +33,12 @@
 #define CAPACITY_SECTORS (DISK_SIZE_BYTES / SECTOR_SIZE)  // 896 секторов
 #define BDISKMAJOR		1
 
+static unsigned long rd_size = 4096;
+
 static int dev_major;
 static struct block_dev *block_device;
 static struct dentry *bd_debugfs_dir;
+static int max_part = 1;
 
 struct block_dev {
     int bd_number;
@@ -45,6 +49,27 @@ struct block_dev {
     struct radix_tree_root bd_pages;
     u64 bd_nr_pages;
 };
+static void bd_submit_bio(struct bio* bio);  // todo убрать в заголовочный файл
+static int bd_rw_page(struct block_device *bdev, sector_t sector, struct page *page, enum req_op op); // todo убрать в заголовочный файл
+
+static const struct block_device_operations bd_fops = {
+	.owner =		THIS_MODULE,
+	.submit_bio =		bd_submit_bio,
+	.rw_page =		bd_rw_page,
+};
+
+/*
+ * bd_submit_bio -- точка входа I/O 
+ * - вызывается блочным слоем при приходе BIO
+ * - перебирает сегменты bio_for_each_segment и вызывает brd_do_bvec
+ * - проверки: выравнивание (offset и len должны быть кратны SECTOR_SIZE)
+ * - особое поведение: если copy_to_brd_setup вернул -ENOMEM и био с REQ_NOWAIT,
+ *   то вызывается bio_wouldblock_error(bio) — правильная реакция для frontends
+ *   которые не хотят ждать.
+ */
+static void bd_submit_bio(struct bio* bio) {
+
+}
 
 /*
  * Найти или выделить block_dev (вставить в глобальный список)
@@ -93,9 +118,10 @@ static struct block_dev* bd_alloc_dev(int n) {
  *   диск через add_disk().
  */
 static int bd_alloc(int n) {
+    int error = -ENOMEM;
     struct block_dev *bd;
     struct gendisk *disk; 
-    char buffer[32]; //DISK_NAME_LEN
+    char buffer[DISK_NAME_LEN]; //DISK_NAME_LEN
 
     bd = bd_alloc_dev(n);
     if (IS_ERR(bd)) {
@@ -105,12 +131,53 @@ static int bd_alloc(int n) {
     spin_lock_init(&bd->bd_lock);
     INIT_RADIX_TREE(&bd->bd_pages, GFP_ATOMIC);
 
-    snprintf(buffer, 32, "bram%d", n);
+    snprintf(buffer, DISK_NAME_LEN, "bram%d", n);
 
+    if (!IS_ERR_OR_NULL(bd_debugfs_dir)) {
+		debugfs_create_u64(buffer, 0444, bd_debugfs_dir,
+				&bd->bd_nr_pages);
+    }
 
+    disk = bd->gdisk = blk_alloc_disk(NUMA_NO_NODE);
 
+    if (!disk) {
+        goto out_free_dev;
+    }
+    disk->major = BDISKMAJOR;
+    disk->first_minor = n * max_part;
+    disk->minors = max_part;
+    disk->fops = &bd_fops;      /* наши операции bio */
+    disk->private_data = bd;  /* быстрый доступ из gendisk -> brd */
+    strscpy(disk->disk_name, buffer, DISK_NAME_LEN);
+    
+    set_capacity(disk, rd_size * 2);  /* rd_size (KB) -> сектора (512B) */
+
+    /*
+        * Установка physical block size в PAGE_SIZE: это сделано, чтобы fdisk
+        * выравнивал partition'ы по 4K (необходимо, если будет использован
+        * direct_access API, возвращающий PFN).
+  */
+    blk_queue_physical_block_size(disk->queue, PAGE_SIZE);
+    /*
+        * Характеристики очереди: не ротационное, не добавлять entropy и NOWAIT.
+    */
+    blk_queue_flag_set(QUEUE_FLAG_NONROT, disk->queue);
+    blk_queue_flag_clear(QUEUE_FLAG_ADD_RANDOM, disk->queue);
+    blk_queue_flag_set(QUEUE_FLAG_NOWAIT, disk->queue);
+    
+    error = add_disk(disk);
+    if (error)   goto cleanup_disk;
 
     return 0;
+
+
+
+cleanup_disk:
+    put_disk(disk);
+
+out_free_dev:
+    
+    return error;
 } 
 static void bd_probe(dev_t dev){
 
