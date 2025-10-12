a1702a424b94631796ac7171052ce6cd9ef9538c
Stanislav <stv@hbuild.minimax.su>
2025-10-12 22:49:47 +0300

    proc test

--- project/src/pr_blk.c
+++ project/src/pr_blk.c
@@ -16,6 +16,93 @@
 #include <linux/debugfs.h>
 #include <linux/version.h>
 #include <linux/uaccess.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+//#include "pr_blk_proc.h"
+
+static struct proc_dir_entry *proc_dir = NULL;
+static struct proc_dir_entry *proc_file = NULL;
+#define PROC_DIR_NAME "bramdev"
+#define PROC_FILE_NAME "stat"
+// buffer for data
+static char *data_buffer = NULL;
+static size_t buffer_size = 0;
+static DEFINE_MUTEX(buffer_mutex);
+
+static int bd_proc_show(struct seq_file *m, void *v) {
+    extern int brd_nr;
+    extern int brd_size;
+    seq_printf(m, "brd_nr: %d\n", brd_nr);
+    seq_printf(m, "brd_size: %d MB\n", brd_size);
+    return 0;
+}
+// Функция открытия файла
+static int proc_open(struct inode *inode, struct file *file) { 
+    return single_open(file, bd_proc_show, NULL); 
+}
+// Функция закрытия файла
+static int proc_release(struct inode *inode, struct file *file) { return 0; }
+
+static ssize_t proc_read(struct file *file, char __user *buf, size_t count,
+                         loff_t *ppos) {
+  ssize_t ret = 0;
+
+  mutex_lock(&buffer_mutex);
+
+  if (*ppos >= buffer_size) {
+    mutex_unlock(&buffer_mutex);
+    return 0; // Конец файла
+  }
+
+  if (*ppos + count > buffer_size)
+    count = buffer_size - *ppos;
+  if (copy_to_user(buf, data_buffer + *ppos, count)) {
+    mutex_unlock(&buffer_mutex);
+    return -EFAULT;
+  }
+
+  *ppos += count;
+  ret = count;
+
+  mutex_unlock(&buffer_mutex);
+  return ret;
+}
+
+static ssize_t proc_write(struct file *file, const char __user *buf,
+                          size_t count, loff_t *ppos) {
+  char *new_buffer = NULL;
+
+  mutex_lock(&buffer_mutex);
+
+  // Освобождаем старый буфер и выделяем новый
+  kfree(data_buffer);
+  data_buffer = NULL;
+  buffer_size = 0;
+  if (copy_from_user(new_buffer, buf, count)) {
+    kfree(new_buffer);
+    mutex_unlock(&buffer_mutex);
+    return -EFAULT;
+  }
+
+  new_buffer[count] = '\0'; // Добавляем нулевой терминатор
+  data_buffer = new_buffer;
+  buffer_size = count;
+
+    mutex_unlock(&buffer_mutex);
+    return count;
+}
+
+// Структура файловых операций
+const struct proc_ops proc_fops = {
+    .proc_open = proc_open,
+    .proc_read = proc_read,
+    .proc_write = proc_write,
+    .proc_release = proc_release,
+};
+
+
+
 
 /* Проверка версии ядра */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6,1,0) || LINUX_VERSION_CODE > KERNEL_VERSION(6,1,255)
@@ -545,6 +632,18 @@ static void brd_cleanup(void)
 
 static int __init mblock_driver_init(void)
 {
+    proc_dir = proc_mkdir(PROC_DIR_NAME, NULL);
+    if (!proc_dir) {
+        pr_warn("Failed to create proc dir\n");
+        return -ENOMEM;
+    }
+    proc_file = proc_create(PROC_FILE_NAME, 0666, proc_dir, &proc_fops);
+    if (!proc_file) {
+        pr_warn("Failed to create proc file\n");
+        remove_proc_entry(PROC_DIR_NAME, NULL);
+        return -ENOMEM;
+    }
+
     bd_debugfs_dir = debugfs_create_dir("ramdisk_pages", NULL);
     int error = 0;
     int major = register_blkdev(BDISKMAJOR, DEVICE_NAME);
@@ -562,6 +661,13 @@ static int __init mblock_driver_init(void)
 
 static void __exit mblock_driver_exit(void)
 {
+    // Удаляем файл и каталог
+    if (proc_file)
+        remove_proc_entry(PROC_FILE_NAME, proc_dir);
+
+    if (proc_dir)
+        remove_proc_entry(PROC_DIR_NAME, NULL);
+    
     unregister_blkdev(BDISKMAJOR, DEVICE_NAME);
     brd_cleanup();
 	pr_info("module unloaded\n");
